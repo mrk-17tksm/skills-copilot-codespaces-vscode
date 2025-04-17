d3141b580f3f2a931b2f17b9b05110182f0a0556
Koki Murakami <kk17mrkm@gmail.com>
2025-04-17 11:50:27 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,17 @@
+function skillsMember()
+{
+    return {
+        name: "Member",
+        description: "A member of the team.",
+        skills: [
+            {
+                name: "JavaScript",
+                level: "Intermediate"
+            },
+            {
+                name: "Python",
+                level: "Beginner"
+            }
+        ]
+    };
+}
